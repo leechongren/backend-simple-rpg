1f75b18657b0dd9824295131e81d6d7daa6c13ea
Chong Ren <leechongren@users.noreply.github.com>
2020-02-19 14:04:42 +0800

    created character model

---
+++ src/models/character.model.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const equipmentSchema = new Schema({
+    armor: {
+        type: String
+    },
+    weapon: {
+        type: String
+    }
+})
+
+const characterSchema = new Schema({
+    id: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    user_id: {
+        type: String,
+        required: true,
+        lowercase: true
+    },
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    job: {
+        type: String,
+        enum: ["Warrior", "Thief", "Mage"],
+        required: true
+    },
+
+    equipments: { equipmentSchema },
+
+    level: {
+        type: Number,
+        default: 1
+    },
+
+    exp: {
+        type: Number,
+        default: 0
+    }
+
+})
+
+const Character = mongoose.model("Character", characterSchema)
+
+module.exports = Character
